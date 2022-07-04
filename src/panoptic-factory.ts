76f58a234b0cbaf58ffd3a2f56b026de79d32840
kkx <qijiongjiang1@hotmail.com>
2022-07-04 11:49:29 +0200

    init thegraph, tracking pool creation

--- src/panoptic-factory.ts
+++ src/panoptic-factory.ts
@@ -1,30 +1,26 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  PanopticPool,
-  OwnershipTransferred
-} from "../generated/PanopticPool/PanopticPool"
-import { ExampleEntity } from "../generated/schema"
+  PanopticFactory,
+  PoolDeployed
+} from "../generated/PanopticPool/PanopticFactory"
+import { PanopticPool } from "../generated/schema"
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+export function handlePoolDeployed(event: PoolDeployed): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = PanopticPool.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new PanopticPool(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
   // Entity fields can be set based on event parameters
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  entity.poolAddress = event.params.poolAddress
+  entity.uniSwapPool  = event.params.uniSwapPool
 
   // Entities can be written to the store with `.save()`
   entity.save()
