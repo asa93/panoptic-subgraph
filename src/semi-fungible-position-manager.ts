4dbc11fdd78d67acd70147285fcea217700b68ba
Slim <asa93@live.fr>
2022-08-13 17:52:18 +0200

    fix : position null error

--- src/semi-fungible-position-manager.ts
+++ src/semi-fungible-position-manager.ts
@@ -1,42 +1,55 @@
-import {BigInt} from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   SemiFungiblePositionManager,
   TokenizedPositionMinted,
   TokenizedPositionBurnt,
-  TokenizedPositionRolled
-} from "../generated/SemiFungiblePositionManager/SemiFungiblePositionManager"
-import {User, TokenPosition} from "../generated/schema"
+  TokenizedPositionRolled,
+} from "../generated/SemiFungiblePositionManager/SemiFungiblePositionManager";
+import { User, TokenPosition } from "../generated/schema";
 
-export function createUserTokenPositionIfNotExisted(id: string, user: User, tokenId: BigInt): TokenPosition {
-  let tokenPosition = TokenPosition.load(id)
+export function createUserTokenPositionIfNotExisted(
+  id: string,
+  user: User,
+  tokenId: BigInt
+): TokenPosition {
+  let tokenPosition = TokenPosition.load(id);
   if (tokenPosition === null) {
-    tokenPosition = new TokenPosition(id)
-    tokenPosition.user = user.id
-    tokenPosition.tokenId = tokenId
-    tokenPosition.save()
+    tokenPosition = new TokenPosition(id);
+    tokenPosition.user = user.id;
+    tokenPosition.tokenId = tokenId;
+    tokenPosition.save();
   }
-  return tokenPosition
+  return tokenPosition;
 }
 
 export function createUserIfNotExisted(userAddress: string): User {
-  let user = User.load(userAddress)
+  let user = User.load(userAddress);
   if (user === null) {
-    user = new User(userAddress)
-    user.save()
+    user = new User(userAddress);
+    user.save();
   }
-  return user
+  return user;
 }
 
-export function handleTokenizedPositionMinted(event: TokenizedPositionMinted): void {
+export function handleTokenizedPositionMinted(
+  event: TokenizedPositionMinted
+): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let user = createUserIfNotExisted(event.params.recipient.toHex())
-  let tokenPosition = createUserTokenPositionIfNotExisted(
-    event.params.tokenId.toString() + "-" + event.params.recipient.toHex(),
-    user, event.params.tokenId
-  )
-  tokenPosition.position = tokenPosition.position + event.params.numberOfContracts
-  tokenPosition.save()
+
+  // let user = createUserIfNotExisted(event.params.recipient.toHex());
+  // let tokenPosition = createUserTokenPositionIfNotExisted(
+  //   event.params.tokenId.toString() + "-" + event.params.recipient.toHex(),
+  //   user,
+  //   event.params.tokenId
+  // );
+
+  let tokenPosition = new TokenPosition(event.params.recipient.toHex());
+  tokenPosition.position = event.params.numberOfContracts;
+  tokenPosition.tokenId = event.params.tokenId;
+  tokenPosition.user = event.params.recipient.toHex();
+  //tokenPosition.endUser = event.params.endUser.toHex();
+  tokenPosition.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -58,33 +71,43 @@ export function handleTokenizedPositionMinted(event: TokenizedPositionMinted): v
   // - contract.owner(...)
 }
 
-export function handleTokenizedPositionBurnt(event: TokenizedPositionBurnt): void {
+export function handleTokenizedPositionBurnt(
+  event: TokenizedPositionBurnt
+): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let user = createUserIfNotExisted(event.params.recipient.toHex())
+  let user = createUserIfNotExisted(event.params.recipient.toHex());
   let tokenPosition = createUserTokenPositionIfNotExisted(
     event.params.tokenId.toString() + "-" + event.params.recipient.toHex(),
-    user, event.params.tokenId
-  )
-  tokenPosition.position = tokenPosition.position - event.params.numberOfContracts
-  tokenPosition.save()
+    user,
+    event.params.tokenId
+  );
+  tokenPosition.position =
+    tokenPosition.position - event.params.numberOfContracts;
+  tokenPosition.save();
 }
 
-export function handleTokenizedPositionRolled(event: TokenizedPositionRolled): void {
+export function handleTokenizedPositionRolled(
+  event: TokenizedPositionRolled
+): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let user = createUserIfNotExisted(event.params.recipient.toHex())
+  let user = createUserIfNotExisted(event.params.recipient.toHex());
   let oldTokenPosition = createUserTokenPositionIfNotExisted(
     event.params.oldTokenId.toString() + "-" + event.params.recipient.toHex(),
-    user, event.params.oldTokenId
-  )
-  oldTokenPosition.position = oldTokenPosition.position - event.params.numberOfContracts
-  oldTokenPosition.save()
+    user,
+    event.params.oldTokenId
+  );
+  oldTokenPosition.position =
+    oldTokenPosition.position - event.params.numberOfContracts;
+  oldTokenPosition.save();
 
   let newTokenPosition = createUserTokenPositionIfNotExisted(
     event.params.newTokenId.toString() + "-" + event.params.recipient.toHex(),
-    user, event.params.newTokenId
-  )
-  newTokenPosition.position = newTokenPosition.position - event.params.numberOfContracts
-  newTokenPosition.save()
+    user,
+    event.params.newTokenId
+  );
+  newTokenPosition.position =
+    newTokenPosition.position - event.params.numberOfContracts;
+  newTokenPosition.save();
 }
