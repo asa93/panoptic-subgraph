ad9ffe6fa74837eecb17b9e4866df1e51bd7a35b
Slim <asa93@live.fr>
2022-08-13 16:49:29 +0200

    config: set block number & network to mainnet

--- src/panoptic-factory.ts
+++ src/panoptic-factory.ts
@@ -1,29 +1,29 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   PanopticFactory,
-  PoolDeployed
-} from "../generated/PanopticFactory/PanopticFactory"
-import { PanopticPool } from "../generated/schema"
+  PoolDeployed,
+} from "../generated/PanopticFactory/PanopticFactory";
+import { PanopticPool } from "../generated/schema";
 
 export function handlePoolDeployed(event: PoolDeployed): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = PanopticPool.load(event.transaction.from.toHex())
+  let entity = PanopticPool.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new PanopticPool(event.transaction.from.toHex())
+    entity = new PanopticPool(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
   }
 
   // Entity fields can be set based on event parameters
-  entity.poolAddress = event.params.poolAddress
-  entity.uniSwapPool  = event.params.uniSwapPool
+  entity.poolAddress = event.params.poolAddress;
+  entity.uniSwapPool = event.params.uniSwapPool;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
